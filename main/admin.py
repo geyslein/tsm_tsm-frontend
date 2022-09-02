08d6f975a3ef03cb4845fc54996599fb651ca3a8
Christian Schulz <christian.schulz@ufz.de>
2022-09-02 13:58:33 +0200

    add stub for validation

--- main/admin.py
+++ main/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django import forms
 
 # Register your models here.
 
@@ -21,6 +22,20 @@ class MqttConfigInline(admin.StackedInline):
     classes = ['collapse']
 
 
+class ThingForm(forms.ModelForm):
+    class Meta:
+        model = Thing
+        fields = ('__all__')
+
+    def clean(self):
+        cleaned_data = super().clean()
+        type = cleaned_data.get("datasource_type")
+
+        #if type == 'SFTP':
+            # TODO
+            # self.add_error('datasource_type', 'Please enter Config for selected datasource type!')
+
+
 class ThingAdmin(admin.ModelAdmin):
     model = Thing
     inlines = [SftpConfigInline, MqttConfigInline, ParserInline]
@@ -30,6 +45,7 @@ class ThingAdmin(admin.ModelAdmin):
             'fields': ('name', 'thing_id', 'database', 'project', 'datasource_type',),
         })
     ]
+    form = ThingForm
 
     def get_readonly_fields(self, request, obj):
         return ['thing_id', 'database',]
@@ -39,6 +55,16 @@ class ThingAdmin(admin.ModelAdmin):
         if request.user.is_superuser:
             return qs
         return qs.filter(userid=request.user)
+
+
+
+    def clean(self):
+        cleaned_data = super().clean()
+        type = cleaned_data.get("datasource_type")
+
+        self.add_error('datasource_type', 'Please enter Config for selected datasource type!')
+
+
 
     def save_model(self, request, obj, form, change):
         if not request.user.is_superuser:
