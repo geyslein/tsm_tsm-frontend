f2351d994c894959677b4559ab4571a5dceb87ec
granseef <florian.gransee@ufz.de>
2023-05-11 11:41:06 +0200

    add logging

--- tsm/admin.py
+++ tsm/admin.py
@@ -50,6 +50,7 @@ def process_thing(sender, instance, **kwargs):
 
     if database is None:
         database = Database()
+        print(os.environ)
         database.url = os.environ.get('CREATEDB_POSTGRES_HOST')
         database.name = os.environ.get('CREATEDB_POSTGRES_DATABASE')
         database.username = create_db_username(thing.group)
